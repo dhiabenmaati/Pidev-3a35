e09481e830b1565ba7e680f47d648060bbd87d19
aminatarkhani95 <amina.tarkhanitarkhani@gmail.com>
2021-03-03 00:33:19 +0100

    Projet syfony

---
+++ src/Entity/Classroom.php
@@ -0,0 +1,108 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ClassroomRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=ClassroomRepository::class)
+ */
+class Classroom
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $name;
+
+    /**
+     * @var
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $nbe;
+
+    /**
+     * @ORM\OneToMany(targetEntity=Student::class, mappedBy="classroom",cascade={"all"},orphanRemoval=true)
+     */
+    private $students;
+
+    public function __construct()
+    {
+        $this->students = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(?string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getNbe()
+    {
+        return $this->nbe;
+    }
+
+    /**
+     * @param mixed $nbe
+     */
+    public function setNbe($nbe): void
+    {
+        $this->nbe = $nbe;
+    }
+
+    /**
+     * @return Collection|Student[]
+     */
+    public function getStudents(): Collection
+    {
+        return $this->students;
+    }
+
+    public function addStudent(Student $student): self
+    {
+        if (!$this->students->contains($student)) {
+            $this->students[] = $student;
+            $student->setClassroom($this);
+        }
+
+        return $this;
+    }
+
+    public function removeStudent(Student $student): self
+    {
+        if ($this->students->contains($student)) {
+            $this->students->removeElement($student);
+            // set the owning side to null (unless already changed)
+            if ($student->getClassroom() === $this) {
+                $student->setClassroom(null);
+            }
+        }
+
+        return $this;
+    }
+
+
+}
---
+++ src/Entity/Club.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ClubRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=ClubRepository::class)
+ */
+class Club
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $ref;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $creationDate;
+
+    public function getRef(): ?int
+    {
+        return $this->ref;
+    }
+
+    public function getCreationDate(): ?string
+    {
+        return $this->creationDate;
+    }
+
+    public function setCreationDate(string $creationDate): self
+    {
+        $this->creationDate = $creationDate;
+
+        return $this;
+    }
+}
